56731cab2f5c24fa8349c18e9ea10d152d6c1f1b
LukyamuziB <lukyamuzibenon@gmail.com>
2017-08-29 02:38:12 +0300

    included routing configuration for the sign up page

--- src/main/java/com/user_frontend/controller/HomeController.java
+++ src/main/java/com/user_frontend/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.user_frontend.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.user_frontend.domain.User;
+import com.user_frontend.service.UserService;
 
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping("/")
 	public String home() {
@@ -30,7 +35,22 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public void signing(@ModelAttribute("user") User user, Model mlodel) {
+	public String signing(@ModelAttribute("user") User user, Model model) {
+		if (userService.checkUserExists(user.getUsername(), user.getEmail())){
+			if (userService.checkUsernameExists(user.getEmail())){
+				model.addAttribute("email Exists",true);
+			}
+			
+			if (userService.checkEmailExists(user.getUsername())){
+				model.addAttribute("user exists",true);
+			}
+			
+			return "signup";
+		}
+		else {
+			userService.save(user);
+			return "redirect:/";
+		}
 		
 	}
 	
